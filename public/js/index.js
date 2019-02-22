15314e38210f505d28078bb7299ba16e1903da16
Mike Wu <mikejcwu@gmail.com>
2019-02-22 13:47:50 -0800

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+socket.on('connect', function(){
+  console.log('Connected to server!');
+
+  // put it inside the connect so it only happens when connected to the server
+  socket.emit('createMessage', {
+    from: 'George Bush',
+    text: 'time to retire'
+  });
+});
+
+socket.on('disconnect', function(){
+  console.log('Disconnected from server!');
+})
+
+socket.on('newMessage', function(message){
+  console.log('New message',message);
+})
